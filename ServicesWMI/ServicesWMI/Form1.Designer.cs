9152ce6a31e053e59c6291de9e74705f03e1fe2c
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-02 11:03:26 -0400

    WMI and C#  - services status
    WMI and C#  - services status

---
+++ ServicesWMI/ServicesWMI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ServicesWMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetServices = new System.Windows.Forms.Button();
+            this.txtServices = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetServices
+            // 
+            this.btnGetServices.Location = new System.Drawing.Point(23, 12);
+            this.btnGetServices.Name = "btnGetServices";
+            this.btnGetServices.Size = new System.Drawing.Size(344, 55);
+            this.btnGetServices.TabIndex = 0;
+            this.btnGetServices.Text = "Get Services Status";
+            this.btnGetServices.UseVisualStyleBackColor = true;
+            this.btnGetServices.Click += new System.EventHandler(this.btnGetServices_Click);
+            // 
+            // txtServices
+            // 
+            this.txtServices.Location = new System.Drawing.Point(23, 73);
+            this.txtServices.Name = "txtServices";
+            this.txtServices.Size = new System.Drawing.Size(344, 268);
+            this.txtServices.TabIndex = 1;
+            this.txtServices.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 351);
+            this.Controls.Add(this.txtServices);
+            this.Controls.Add(this.btnGetServices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetServices;
+        private System.Windows.Forms.RichTextBox txtServices;
+    }
+}
+
---
+++ ServicesWMI/ServicesWMI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServicesWMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public String Get_Service_Status()
+        {
+
+            String result = String.Empty;
+
+            ManagementScope scope = null;
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions();
+                options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+                //options.Username = "username"
+                //options.Password = "password"
+                //options.Authority = "NTLMDOMAIN:MY_DOMAIN";
+
+                scope = new ManagementScope("\\\\" +"PCNAME"+ "\\root\\cimv2", options);
+                scope.Connect();
+
+                //Query system for Operating System information
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Service");
+                //ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Service WHERE Name = 'Tomcat6'");
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                ManagementObjectCollection queryCollection = searcher.Get();
+                foreach (ManagementObject m in queryCollection)
+                {
+                    result = result + "Service: " + m["Name"].ToString() + " Status: " + m["State"].ToString() + "\r";
+                }
+
+
+            }
+            catch 
+            {
+                result = "Unable to Retrieve Service Status";
+            }
+
+            scope = null;
+
+            return result;
+
+
+        }
+
+        private void btnGetServices_Click(object sender, EventArgs e)
+        {
+            txtServices.Text= Get_Service_Status();
+        }
+    }
+}
+
